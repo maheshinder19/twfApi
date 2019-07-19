e2ce2cd0385ba6073197f21df9c17830eaaa5dec
maheshinder19 <mahesh.inder85@gmail.com>
2019-07-19 16:41:40 +0530

    handle empty order

--- app.js
+++ app.js
@@ -33,7 +33,6 @@ app.use("/order", orderRoute);
 app.use(function(req, res, next) {
   var err = new Error("Not Found");
   err.status = 404;
-
   next(err);
 });
 
@@ -41,9 +40,7 @@ app.use(function(req, res, next) {
 
 app.use(function(err, req, res, next) {
   res.status(err.status || 500);
-  res.json({
-    message: err.message
-  });
+  res.json(err.message);
   next();
 });
 
--- orderRoute.js
+++ orderRoute.js
@@ -7,9 +7,14 @@ const express = require("express"),
 //get  minimum cost
 
 router.get("/", (req, res, next) => {
-  const minCost = findMinCost(req.body.order);
-  res.send(`Minimum cost is ${minCost}`);
-  console.log(`Minimum cost is ${minCost}`);
+  if (req.body.order == undefined) {
+    next({ message: "Please provide valid order input!" });
+  } else {
+    const minCost = findMinCost(req.body.order);
+
+    res.send(`Minimum cost is ${minCost}`);
+    console.log(`Minimum cost is ${minCost}`);
+  }
 });
 
 module.exports = router;
